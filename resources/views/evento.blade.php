b2fa2856ad92382971c4d70cf0b6204ee45b2150
4lecky <lunitaromero1221@gmail.com>
2025-10-21 16:56:54 -0500

    Conexión a base de datos

---
+++ resources/views/evento.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+
+<body>
+
+    <div class="seccionForm">
+
+        <form action="" class="container_form">
+
+            <div class="campo">
+                <label for="" class="label_style">Nombre del evento</label>
+                <input type="number" placeholder="ingrese su numero de telefono" class="input_style">
+
+                <label for="" class="label_style">Descripción del evento</label>
+                <textarea name="descripción" id="" class="textA_style"></textarea>
+                <!-- <input type="text" placeholder="ingrese su numero de telefono" class="input_style"> -->
+            </div>
+
+            <div class="campo">
+                <label for="" class="label_style">Titulo campo</label>
+                <input type="datetime-local" placeholder="Fecha y hora de fin" class="input_style">
+
+                <label for="" class="label_style">Titulo campo</label>
+                <input type="datetime-local" placeholder="ingrese su numero de telefono" class="input_style">
+            </div>
+        </form>
+
+    </div>
+
+</body>
+
+</html>
