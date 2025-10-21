2acaa64db52dfffb0749d6bc54c19767880195d1
darlymarroquin25-15 <darlymarroquin14@gmail.com>
2025-10-21 17:00:04 -0500

    Guardo mis cambios locales en web.php

---
+++ app/Http/Controllers/LocalidadesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Localidades;
+use Illuminate\Http\Request;
+
+class LocalidadesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Localidades $localidades)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Localidades $localidades)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Localidades $localidades)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Localidades $localidades)
+    {
+        //
+    }
+}
