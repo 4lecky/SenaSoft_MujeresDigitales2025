a4c85e56872ffbe493d95d5293f9d7f7ce103e6b
4lecky <lunitaromero1221@gmail.com>
2025-10-21 19:41:03 -0500

    Formulario con estilos

--- resources/views/eventos/create.blade.php
+++ resources/views/eventos/create.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- <link rel="stylesheet" href="../../eventos.css"> -->
-    <link rel="stylesheet" href="{{ asset('sena/app/resources/css/eventos/eventos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/eventos.css') }}">
 
 </head>
 
--- resources/views/localidades/create.blade.php
+++ resources/views/localidades/create.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- <link rel="stylesheet" href="../../eventos.css"> -->
-    <link rel="stylesheet" href="{{ asset('sena/app/resources/css/eventos/eventos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/eventos.css') }}">
 
 </head>
 
@@ -18,22 +18,16 @@
          @csrf
 
             <div class="campo">
-                <label for="" class="label_style">Nombre del evento</label>
-                <input type="text" placeholder="ingrese su numero de telefono" class="input_style" name='nombre'>
+                <label for="" class="label_style">Nombre del localidad</label>
+                <input type="text" placeholder="ingrese el nombre de la localidad" class="input_style" name='nombre'>
 
-                <label for="" class="label_style">Descripción del evento</label>
-                <select name="estados" id=""></select>
-                <option value="General">General</option>
-                <option value="Preferencial">Preferencial</option>
-                <option value="VIP">VIP</option>
-            </div>
-
-            <div class="campo">
-                <label for="" class="label_style">Titulo campo</label>
-                <input type="datetime-local" placeholder="Fecha y hora de fin" class="input_style" name='horaFecha_inicio'>
-
-                <label for="" class="label_style">Titulo campo</label>
-                <input type="datetime-local" placeholder="ingrese su numero de telefono" class="input_style" name='horaFecha_fin'>
+                <label for="" class="label_style" >Descripción del evento</label>
+                    <select name="estados" id="">
+                    <option value="">Seleccione una opción</option>
+                    <option value="General">General</option>
+                    <option value="Preferencial">Preferencial</option>
+                    <option value="VIP">VIP</option>
+                </select>
             </div>
 
             <button> Enviar </button>
