c62c2d56408ac385a8aa5dc64e0bf2b3fbf9c10a
4lecky <lunitaromero1221@gmail.com>
2025-10-22 11:36:16 -0500

    Estilos mejorados

--- resources/views/eventos/create.blade.php
+++ resources/views/eventos/create.blade.php
@@ -5,21 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link rel="stylesheet" href="../../eventos.css"> -->
+    <!-- Css -->
     <link rel="stylesheet" href="{{ asset('css/eventos.css') }}">
-
+    <!-- Iconos -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
 </head>
 
 <body>
 
-    <div class="seccionForm" >
+    <div class="sectionIzquierda" >
+
+        <h3 class='title_principal'> Creación de eventos </h3>
 
         <form action="{{ route('eventos.store') }}" class="container_form" method='POST'>
          @csrf
 
             <div class="campo">
                 <label for="" class="label_style">Nombre del evento</label>
-                <input type="text" placeholder="ingrese su numero de telefono" class="input_style" name='nombre'>
+                <i class="ri-disc-line"></i>
+                <input type="text" placeholder="Ingrese el nombre del evento" class="input_style" name='nombre' require>
 
                 <label for="" class="label_style">Descripción del evento</label>
                 <textarea name="descripcion" id="" class="textA_style"></textarea>
@@ -27,15 +31,24 @@
             </div>
 
             <div class="campo">
-                <label for="" class="label_style">Titulo campo</label>
-                <input type="datetime-local" placeholder="Fecha y hora de fin" class="input_style" name='horaFecha_inicio'>
+                <label for="" class="label_style">Hora y fecha de incio</label>
+                <input type="datetime-local" placeholder="Fecha y hora de fin" class="input_style" name='horaFecha_inicio' require>
 
-                <label for="" class="label_style">Titulo campo</label>
-                <input type="datetime-local" placeholder="ingrese su numero de telefono" class="input_style" name='horaFecha_fin'>
+                <label for="" class="label_style">Hora y fecha de incio</label>
+                <input type="datetime-local" placeholder="ingrese su numero de telefono" class="input_style" name='horaFecha_fin' require>
             </div>
 
-            <button> Enviar </button>
+            <div class="containerBtn">
+                <button> Enviar <i class="ri-album-fill"></i></button>
+            </div>
         </form>
+
+    </div>
+
+    <div class="sectionDerecha">
+
+        <img src="{{ asset('img/cassets_fondo.jpg') }}" alt="" class='img_fondo'>
+
 
     </div>
 
